9aec3eb15c25b0d378018bebda501fac9ae7bc1c
lorrain <lorrain0407@gmail.com>
2017-02-16 22:26:19 +0800

    Add I/O operators

--- 3DQrcode/3D_Qrcode/main.cpp
+++ 3DQrcode/3D_Qrcode/main.cpp
@@ -1,36 +1,64 @@
 #include <igl/readOFF.h>
+#include <igl/writeOFF.h>
 #include <igl/viewer/Viewer.h>
-#include <igl/jet.h>
+#include <nanogui/formhelper.h>
+#include <nanogui/screen.h>
+#include "igl/file_dialog_open.h"
+#include "igl/file_dialog_save.h"
+//#include <igl/jet.h>
 #include <iostream>
+
 using namespace std;
 //#include "tutorial_shared_path.h"
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
-Eigen::MatrixXd C;
+//Eigen::MatrixXd C;
 
 int main(int argc, char *argv[])
 {
-  // Load a mesh in OFF format
-  //igl::readOFF(TUTORIAL_SHARED_PATH "/screwdriver.off", V, F);
-
-  // Plot the mesh
+  // Initiate viewer
   igl::viewer::Viewer viewer;
-  //viewer.data.set_mesh(V, F);
 
-  // Use the z coordinate as a scalar field over the surface
-  //Eigen::VectorXd Z = V.col(2);
+  viewer.callback_init = [&](igl::viewer::Viewer& viewer)
+  {
+	  // Add new group
+	  viewer.ngui->addGroup("Load & Save");
 
-  // Compute per-vertex colors
-  //igl::jet(Z,true,C);
+	  // Add a button
+	  viewer.ngui->addButton("Load Mesh", [&]() { 
+		  viewer.data.clear();
+		  string InputFile = "";
+		  const char *input;
+		  InputFile = igl::file_dialog_open();
+		  input = InputFile.c_str();
+		  if (InputFile != "")
+		  {
+			  viewer.load_mesh_from_file(input);
+			  //igl::readOFF(InputFile, V, F);
+			  viewer.data.set_mesh(V, F);
+		  }
+		  
+	  });
 
-  // Add per-vertex colors
-  //viewer.data.set_colors(C);
+	  viewer.ngui->addButton("Save Mesh", [&]() {
+		  string OutputFile = "";
+		  const char *output;
+		  OutputFile = igl::file_dialog_save();
+		  output = OutputFile.c_str();
+		  if (OutputFile != "")
+		  {
+			  viewer.save_mesh_to_file(output);
+		  }
+	  });
 
-  cout << "hello 3d qrcode" << endl;
+	  // Generate menu
+	  viewer.screen->performLayout();
+
+	  return false;
+  };
 
   // Launch the viewer
   viewer.launch();
 
-  
 }
