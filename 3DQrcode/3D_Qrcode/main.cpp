8afef6856d6964b48c63dec0ea41c60d0f59f7de
lorrain <lorrain0407@gmail.com>
2017-02-17 22:48:24 +0800

    Add save mesh with 'stl' format and select triangular facet of mesh

--- 3DQrcode/3D_Qrcode/main.cpp
+++ 3DQrcode/3D_Qrcode/main.cpp
@@ -1,25 +1,34 @@
+#include "SaveMesh.h"
+
+#include "igl/file_dialog_open.h"
+#include "igl/file_dialog_save.h"
+#include <Eigen/Dense>
 #include <igl/readOFF.h>
 #include <igl/writeOFF.h>
 #include <igl/viewer/Viewer.h>
 #include <nanogui/formhelper.h>
 #include <nanogui/screen.h>
-#include "igl/file_dialog_open.h"
-#include "igl/file_dialog_save.h"
+#include <igl/unproject_onto_mesh.h>
 //#include <igl/jet.h>
+
 #include <iostream>
 
 using namespace std;
-//#include "tutorial_shared_path.h"
-
-Eigen::MatrixXd V;
-Eigen::MatrixXi F;
-//Eigen::MatrixXd C;
+using namespace igl;
 
 int main(int argc, char *argv[])
 {
   // Initiate viewer
   igl::viewer::Viewer viewer;
 
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  Eigen::MatrixXd C;
+  //V.resize(0, 0);
+  //F.resize(0, 0);
+  //C.resize(0, 0);
+
+  // UI Design
   viewer.callback_init = [&](igl::viewer::Viewer& viewer)
   {
 	  // Add new group
@@ -27,20 +36,25 @@ int main(int argc, char *argv[])
 
 	  // Add a button
 	  viewer.ngui->addButton("Load Mesh", [&]() { 
-		  viewer.data.clear();
+		  //viewer.data.clear();
 		  string InputFile = "";
 		  const char *input;
 		  InputFile = igl::file_dialog_open();
 		  input = InputFile.c_str();
 		  if (InputFile != "")
 		  {
+			  viewer.data.clear();
 			  viewer.load_mesh_from_file(input);
 			  //igl::readOFF(InputFile, V, F);
-			  viewer.data.set_mesh(V, F);
-		  }
-		  
+			  V = viewer.data.V;
+			  F = viewer.data.F;
+			  // Initialize white
+			  C = Eigen::MatrixXd::Constant(F.rows(), 3, 1);
+			  viewer.data.set_colors(C);
+		  }  
 	  });
 
+	  // Add a button
 	  viewer.ngui->addButton("Save Mesh", [&]() {
 		  string OutputFile = "";
 		  const char *output;
@@ -48,13 +62,38 @@ int main(int argc, char *argv[])
 		  output = OutputFile.c_str();
 		  if (OutputFile != "")
 		  {
-			  viewer.save_mesh_to_file(output);
+			  //cout << OutputFile;
+			  SaveMesh(output,viewer,viewer.data);
+			  //viewer.save_mesh_to_file(output);
 		  }
 	  });
 
 	  // Generate menu
 	  viewer.screen->performLayout();
 
+	  return false;
+  };
+
+  // Select Target Region
+
+
+  viewer.callback_mouse_down =
+	  [&V, &F, &C](igl::viewer::Viewer& viewer, int, int)->bool
+  {
+	  int fid;
+	  Eigen::Vector3f bc;
+	  // Cast a ray in the view direction starting from the mouse position
+	  double x = viewer.current_mouse_x;
+	  double y = viewer.core.viewport(3) - viewer.current_mouse_y;
+	  if (igl::unproject_onto_mesh(Eigen::Vector2f(x, y), viewer.core.view * viewer.core.model,
+		  viewer.core.proj, viewer.core.viewport, V, F, fid, bc))
+	  {
+		  // paint hit red
+		  C.row(fid) << 1, 0, 0;
+		  cout << fid << endl;
+		  viewer.data.set_colors(C);
+		  return true;
+	  }
 	  return false;
   };
 
